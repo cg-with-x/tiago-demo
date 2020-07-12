4ff08d572a8c8a7b0ae221ee4dd354297157e566
zhangwenjiang <zhangwenjiang@bytedance.com>
2020-07-12 16:22:32 +0800

    feat: add joinRTCForGameRoom when reconnect

--- client/assets/scripts/game.js
+++ client/assets/scripts/game.js
@@ -1,115 +1,125 @@
-import roomManager from './room_manager';
+import roomManager from "./room_manager";
+import tiago from "@byted-creative/tiago";
 
-import utils from './utils';
-import dataManager from './data_manager';
+import utils from "./utils";
+import dataManager from "./data_manager";
 
 cc.Class({
-    extends: cc.Component, 
+  extends: cc.Component,
 
-    playerAOpenId: '',   
-    playerBOpenId: '',
+  playerAOpenId: "",
+  playerBOpenId: "",
 
-    properties: {
-        labelPlayerATip: cc.Label,
-        labelPlayerANickName: cc.Label,
-        spritePlayerAAvatar: cc.Sprite,
+  properties: {
+    labelPlayerATip: cc.Label,
+    labelPlayerANickName: cc.Label,
+    spritePlayerAAvatar: cc.Sprite,
 
-        labelPlayerBTip: cc.Label,
-        labelPlayerBNickName: cc.Label,
-        spritePlayerBAvatar: cc.Sprite,
+    labelPlayerBTip: cc.Label,
+    labelPlayerBNickName: cc.Label,
+    spritePlayerBAvatar: cc.Sprite,
 
-        labelServerTime: cc.Label,
-    },
+    labelServerTime: cc.Label,
+  },
 
-    start () {
+  start() {},
 
-    },
+  onClickStopGame() {
+    roomManager.room.send(
+      JSON.stringify({
+        event: "bye",
+      })
+    );
+    roomManager.leave();
+    cc.director.loadScene("start");
+  },
 
-    onClickStopGame() {
-        roomManager.room.send(JSON.stringify({
-            event: 'bye',
-        }));
-        roomManager.leave();
-        cc.director.loadScene('start');
-    },
-
-    onClickTalk() {
-        if (roomManager.room) {
-            roomManager.room.send(JSON.stringify({
-                event: 'talk',
-                data: parseInt(Math.random() * 100),
-            }));
-        }
-    },
-
-    onClickReconnect() {
-        if (roomManager.room) {
-            roomManager.room.reconnect();
-        }
-    },
-
-    onRoomMessage(messageStr) {
-        // NOTE: 消息体的内容是开发者自己定义的，这里的代码只是一种示例
-        // NOTE: 开发者可以根据自己的房间脚本和协议，实现自身游戏的逻辑
-        const message = JSON.parse(messageStr);
-
-        if (message.length) {
-            message.forEach(({ event, data }) => {
-                switch (event) {
-                    case 'game-start':
-                        break;
-                    case 'environment':
-                        dataManager.environment = data;
-                        break;
-                    case 'info':
-                        dataManager.twoPlayersInfo = data;
-                        this.renderPlayers();
-                        break;
-                    case 'server-time':
-                        this.labelServerTime.string = `${dataManager.environment}: ${data}`;
-                        break;
-                    case 'talk':
-                        this.renderTalk(data);
-                        break;
-                    case 'game-over':
-                        roomManager.leave();
-                        cc.director.loadScene('start');
-                        break;
-                    default:
-                        break;
-                }
-            })
-        }
-    },
-
-    renderPlayers() {
-        if (dataManager.twoPlayersInfo) {
-            const [ playerA, playerB ] = dataManager.twoPlayersInfo;
-
-            if (playerA) {
-                this.playerAOpenId = playerA.openId;
-                const name = playerA.isAI ? `AI: ${playerA.nickName}` : playerA.nickName;
-                this.labelPlayerANickName.string = name;
-                utils.renderAvatar(this.spritePlayerAAvatar, playerA.avatarUrl);
-            }
-
-            if (playerB) {
-                this.playerBOpenId = playerB.openId;
-                const name = playerB.isAI ? `AI: ${playerB.nickName}` : playerB.nickName;
-                this.labelPlayerBNickName.string = name;
-                utils.renderAvatar(this.spritePlayerBAvatar, playerB.avatarUrl);
-            }
-        }
-    },
-
-    renderTalk({ openId, data }) {
-        const tip = `战斗力 +${data}`;
-        if (openId === this.playerAOpenId) {
-            this.labelPlayerATip.string = tip;
-        } else if (openId === this.playerBOpenId) {
-            this.labelPlayerBTip.string = tip;
-        }
+  onClickTalk() {
+    if (roomManager.room) {
+      roomManager.room.send(
+        JSON.stringify({
+          event: "talk",
+          data: parseInt(Math.random() * 100),
+        })
+      );
     }
+  },
 
-    // update (dt) {},
+  onClickReconnect() {
+    if (roomManager.room) {
+      roomManager.room.reconnect();
+
+      // NOTE: 加入房间连麦
+      tiago.joinRTCForGameRoom(roomManager.room);
+    }
+  },
+
+  onRoomMessage(messageStr) {
+    // NOTE: 消息体的内容是开发者自己定义的，这里的代码只是一种示例
+    // NOTE: 开发者可以根据自己的房间脚本和协议，实现自身游戏的逻辑
+    const message = JSON.parse(messageStr);
+
+    if (message.length) {
+      message.forEach(({ event, data }) => {
+        switch (event) {
+          case "game-start":
+            break;
+          case "environment":
+            dataManager.environment = data;
+            break;
+          case "info":
+            dataManager.twoPlayersInfo = data;
+            this.renderPlayers();
+            break;
+          case "server-time":
+            this.labelServerTime.string = `${dataManager.environment}: ${data}`;
+            break;
+          case "talk":
+            this.renderTalk(data);
+            break;
+          case "game-over":
+            roomManager.leave();
+            cc.director.loadScene("start");
+            break;
+          default:
+            break;
+        }
+      });
+    }
+  },
+
+  renderPlayers() {
+    if (dataManager.twoPlayersInfo) {
+      const [playerA, playerB] = dataManager.twoPlayersInfo;
+
+      if (playerA) {
+        this.playerAOpenId = playerA.openId;
+        const name = playerA.isAI
+          ? `AI: ${playerA.nickName}`
+          : playerA.nickName;
+        this.labelPlayerANickName.string = name;
+        utils.renderAvatar(this.spritePlayerAAvatar, playerA.avatarUrl);
+      }
+
+      if (playerB) {
+        this.playerBOpenId = playerB.openId;
+        const name = playerB.isAI
+          ? `AI: ${playerB.nickName}`
+          : playerB.nickName;
+        this.labelPlayerBNickName.string = name;
+        utils.renderAvatar(this.spritePlayerBAvatar, playerB.avatarUrl);
+      }
+    }
+  },
+
+  renderTalk({ openId, data }) {
+    const tip = `战斗力 +${data}`;
+    if (openId === this.playerAOpenId) {
+      this.labelPlayerATip.string = tip;
+    } else if (openId === this.playerBOpenId) {
+      this.labelPlayerBTip.string = tip;
+    }
+  },
+
+  // update (dt) {},
 });
--- client/assets/scripts/start.js
+++ client/assets/scripts/start.js
@@ -1,139 +1,144 @@
-import tiago from '@byted-creative/tiago';
-import roomManager from './room_manager';
-import dataManager from './data_manager';
-import utils from './utils';
+import tiago from "@byted-creative/tiago";
+import roomManager from "./room_manager";
+import dataManager from "./data_manager";
+import utils from "./utils";
 
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        labelNickName: cc.Label,
-        spriteAvatar: cc.Sprite,
-        nodeFeature: cc.Node,
-        nodeLoading: cc.Node,
-    },
+  properties: {
+    labelNickName: cc.Label,
+    spriteAvatar: cc.Sprite,
+    nodeFeature: cc.Node,
+    nodeLoading: cc.Node,
+  },
 
-    onLoad () {
-        if (!dataManager.tiagoInited) {
-            this.nodeFeature.active = false;
-            this.nodeLoading.active = true;
+  onLoad() {
+    if (!dataManager.tiagoInited) {
+      this.nodeFeature.active = false;
+      this.nodeLoading.active = true;
 
-            tiago.init({
-                appId: 'ttbad4cc6925eea862',
-                debug: true,
-            }).then(() => {
-                console.log('tiago init success.');
-                dataManager.tiagoInited = true;
+      tiago
+        .init({
+          appId: "ttbad4cc6925eea862",
+          debug: true,
+        })
+        .then(() => {
+          console.log("tiago init success.");
+          dataManager.tiagoInited = true;
 
-                this.nodeFeature.active = true;
-                this.nodeLoading.active = false;
-            }).catch(err => {
-                console.log(err);
-                // NOTE: 一般情况不会出错，我们会对错误情况做监控，游戏可以处理一些异常情况下的表现
-            });
-        } else {
-            this.nodeFeature.active = true;
-            this.nodeLoading.active = false;
-        }
-
-        console.log('loaded')
-    },
-
-    start () {
-        if (dataManager.selfUserInfo) {
-            this.renderSelf(dataManager.selfUserInfo);
-        }
-    },
-
-    renderSelf(info) {
-        this.labelNickName.string = info.nickName;
-        utils.renderAvatar(this.spriteAvatar, info.avatarUrl);
-    },
-
-    onClickGetConfig() {
-        const { BUSINESS_SCENE } = tiago;   
-
-        const config = tiago.getConfig();
-        console.log(config);
-
-        const { currentScene, scene } = config;
-
-        if (currentScene === BUSINESS_SCENE.LiveRoom) {
-            // 当前是直播场景
-            const params = scene[currentScene];
-
-            if (params.isNewcomer) {  
-                // NOTE: 补充新手逻辑
-                console.log('新手首次加入游戏');
-            }
-            
-            // NOTE: 针对直播场景，调整使用直播专用的 UI，或处理其他特殊逻辑
-        } else if (currentScene === BUSINESS_SCENE.Wonderland) {
-            // 当前是 W 场景
-        }
-    },
-
-    onClickGetUserInfo() {
-        const info = tiago.getUserInfo();
-        console.log(info);
-
-        // NOTE: 这里只是简单的保存下来
-        dataManager.selfUserInfo = info;
-        this.renderSelf(dataManager.selfUserInfo);
-    },
-
-    onClickStartSingleMatch() {
-        const match = tiago.startSingleMatch();
-
-        match.on('match-success', result => {
-            // 获得匹配成功后的用户信息
-            console.log(result);
+          this.nodeFeature.active = true;
+          this.nodeLoading.active = false;
+        })
+        .catch((err) => {
+          console.log(err);
+          // NOTE: 一般情况不会出错，我们会对错误情况做监控，游戏可以处理一些异常情况下的表现
         });
-        
-        match.on('create-game-room-success', result => {
-            console.log(result);
+    } else {
+      this.nodeFeature.active = true;
+      this.nodeLoading.active = false;
+    }
 
-            // NOTE: 随后可以加入游戏房间
-            const room = tiago.joinGameRoom({
-                roomNum: result.roomNum,
-            });
-            
-            // NOTE: 加入房间连麦
-            tiago.joinRTCForGameRoom(room);
+    console.log("loaded");
+  },
 
-            // 交由 room_manager 进行管理
-            roomManager.loadRoom(room);
-        });
-        
-        match.on('error', error => {
-            console.log(error);
-        });
-    },
+  start() {
+    if (dataManager.selfUserInfo) {
+      this.renderSelf(dataManager.selfUserInfo);
+    }
+  },
 
-    onClickStartSingleMatchAI() {
-        const match = tiago.startSingleMatch({
-            isAutoAppendAI: true, // 支持 AI 逻辑
-        });
+  renderSelf(info) {
+    this.labelNickName.string = info.nickName;
+    utils.renderAvatar(this.spriteAvatar, info.avatarUrl);
+  },
 
-        match.on('match-success', result => {
-            // 获得匹配成功后的用户信息
-            console.log(result);
-        });
-        
-        match.on('create-game-room-success', result => {
-            console.log(result);
+  onClickGetConfig() {
+    const { BUSINESS_SCENE } = tiago;
 
-            // NOTE: 随后可以加入游戏房间
-            const room = tiago.joinGameRoom({
-                roomNum: result.roomNum,
-            });
+    const config = tiago.getConfig();
+    console.log(config);
 
-            // 交由 room_manager 进行管理
-            roomManager.loadRoom(room);
-        });
-        
-        match.on('error', error => {
-            console.log(error);
-        });
-    },
+    const { currentScene, scene } = config;
+
+    if (currentScene === BUSINESS_SCENE.LiveRoom) {
+      // 当前是直播场景
+      const params = scene[currentScene];
+
+      if (params.isNewcomer) {
+        // NOTE: 补充新手逻辑
+        console.log("新手首次加入游戏");
+      }
+
+      // NOTE: 针对直播场景，调整使用直播专用的 UI，或处理其他特殊逻辑
+    } else if (currentScene === BUSINESS_SCENE.Wonderland) {
+      // 当前是 W 场景
+    }
+  },
+
+  onClickGetUserInfo() {
+    const info = tiago.getUserInfo();
+    console.log(info);
+
+    // NOTE: 这里只是简单的保存下来
+    dataManager.selfUserInfo = info;
+    this.renderSelf(dataManager.selfUserInfo);
+  },
+
+  onClickStartSingleMatch() {
+    const match = tiago.startSingleMatch();
+
+    match.on("match-success", (result) => {
+      // 获得匹配成功后的用户信息
+      console.log(result);
+    });
+
+    match.on("create-game-room-success", (result) => {
+      console.log(result);
+
+      // NOTE: 随后可以加入游戏房间
+      const room = tiago.joinGameRoom({
+        roomNum: result.roomNum,
+      });
+
+      // NOTE: 加入房间连麦
+      tiago.joinRTCForGameRoom(room);
+
+      // 交由 room_manager 进行管理
+      roomManager.loadRoom(room);
+    });
+
+    match.on("error", (error) => {
+      console.log(error);
+    });
+  },
+
+  onClickStartSingleMatchAI() {
+    const match = tiago.startSingleMatch({
+      isAutoAppendAI: true, // 支持 AI 逻辑
+    });
+
+    match.on("match-success", (result) => {
+      // 获得匹配成功后的用户信息
+      console.log(result);
+    });
+
+    match.on("create-game-room-success", (result) => {
+      console.log(result);
+
+      // NOTE: 随后可以加入游戏房间
+      const room = tiago.joinGameRoom({
+        roomNum: result.roomNum,
+      });
+      // NOTE: 加入房间连麦
+      tiago.joinRTCForGameRoom(room);
+
+      // 交由 room_manager 进行管理
+      roomManager.loadRoom(room);
+    });
+
+    match.on("error", (error) => {
+      console.log(error);
+    });
+  },
 });
